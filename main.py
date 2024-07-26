722514746fc425b0fb8eb85b5b22e727346cb942
Davi <davimoreira58@gmail.com>
2024-07-26 02:17:04 -0300

    adicionando delays para o carregamento da pagina

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ pyautogui.write('https://dlp.hashtagtreinamentos.com/python/intensivao/login')
 pyautogui.press('enter')
 
 
+time.sleep(3)
 pyautogui.click(x=956, y=372)
 pyautogui.hotkey('crtl' + 'a')
 pyautogui.write('email@gmail.com')
@@ -25,3 +26,5 @@ pyautogui.hotkey('crtl' + 'a')
 pyautogui.write('senha')
 pyautogui.press('tab')
 pyautogui.press('enter')
+time.sleep(3)
+
