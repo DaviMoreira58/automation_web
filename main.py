34623fefc9a69f683006b7dd2bf389abb448e715
Davi <davimoreira58@gmail.com>
2024-07-26 02:16:06 -0300

    fazendo login no site

--- main.py
+++ main.py
@@ -15,3 +15,13 @@ time.sleep(1)
 pyautogui.click(x=932, y=57)
 pyautogui.write('https://dlp.hashtagtreinamentos.com/python/intensivao/login')
 pyautogui.press('enter')
+
+
+pyautogui.click(x=956, y=372)
+pyautogui.hotkey('crtl' + 'a')
+pyautogui.write('email@gmail.com')
+pyautogui.press('tab')
+pyautogui.hotkey('crtl' + 'a')
+pyautogui.write('senha')
+pyautogui.press('tab')
+pyautogui.press('enter')
