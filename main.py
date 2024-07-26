41bc392680f113f18393af8b69f788371f4bd6be
Davi <davimoreira58@gmail.com>
2024-07-26 02:14:55 -0300

    primeiro passo abir o navegador e o site

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ pyautogui.PAUSE = 0.5
 
 source = os.path.abspath('.')
 path = os.path.join(source, 'produtos.csv')
+
+pyautogui.press("win")
+pyautogui.write('chrome')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.click(x=932, y=57)
+pyautogui.write('https://dlp.hashtagtreinamentos.com/python/intensivao/login')
+pyautogui.press('enter')
